97433407f69a7958d8c8475d009aaeaeae1bc5f9
yangming2312 <yangming2312@163.com>
2017-09-09 00:25:04 +0800

    levelbar done

--- src/views/layout/levelbar.vue
+++ src/views/layout/levelbar.vue
@@ -1,10 +1,11 @@
-<!--面包宵组件-->
+<!--面包屑组件-->
 <template>
   <el-breadcrumb separator="/" class="app-levelBar">
-    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-    <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-    <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+    <el-breadcrumb-item v-for="(item, index) in levelList" :key="item.path">
+      <!--不使用的节点-->
+       <span v-if="item.redirect === 'noredirect'|| index === item.length - 1" class="no-redirect">{{item.name}}</span>
+       <router-link v-else :to="item.redirect || item.path">{{item.name}}</router-link>
+    </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
@@ -20,7 +21,28 @@
     },
     methods: {
       getBreadcrumb() {
+        let matched = this.$route.matched.filter(item => item.name);
+        const first = matched[0];
+        if (first && (first.name !== '首页' || first.path !== '')) {
+          matched = [{ name: '首页', path: '/' }].concat(matched);
+        }
+        this.levelList = matched;
+        console.log(matched);
+      }
+    },
+    watch: {    // 监测路由变化
+      $route() {
+        this.getBreadcrumb();
       }
     }
   };
 </script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .app-levelBar .el-breadcrumb {
+    .no-redirect {
+      color: #97a8be;
+      cursor: text;
+    }
+  }
+</style>
